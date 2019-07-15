7c41a593da1d9d4c59550e987bac0d56c16d620c
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-07-14 20:52:55 -0700

    another approach 2

--- countAndSay.js
+++ countAndSay.js
@@ -1,15 +1,16 @@
 https://leetcode.com/problems/count-and-say/
 
 var countAndSay = function(n) {
-    var count = 1;
+    var prev = '1';
     var say = '';
-    i = 0
+    i = 0;
     while (i < n) {
-        
-        say = count
-        
-        i++
-    }
+        var counter = 1;
+        // look at first number check if next number is the same
+        say = prev;
+        prev = counter + say;
+        i++;
+    }; 
     
-    return say
+    return say;
 };
